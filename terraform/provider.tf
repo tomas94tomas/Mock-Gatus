b63215d863d7be7b3a998c855bb25418187a88a1
tomas94tomas <tomas011@gmail.com>
2025-08-26 20:48:05 +0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,1 +1,12 @@
+terraform {
+  required_version = ">= 1.6"
+  required_providers {
+    aws      = { source = "hashicorp/aws", version = "~> 5.0" }
+    template = { source = "hashicorp/template", version = "~> 2.2" }
+  }
+  backend "s3" {}
+}
 
+provider "aws" {
+  region = var.region
+}
