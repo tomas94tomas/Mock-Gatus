684f89b2a528bd18de69ecd190d5b60248fd8aad
JancikK <yonaskollegaev@gmail.com>
2025-08-28 19:36:13 +0300

    backend updated

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform { backend "s3" {|
-  bucket         = "ca-devops-ua5-terraform-state"
-    key            = "terraform/48/terraform.tfstate"
+  bucket         = "devops-project-terraform-state-2025"
+    key            = "teraform/devops-project-terraform-state-2025/terraform.tfstate"
     region         = "eu-central-1"
     use_lockfile   = true
     encrypt        = true} 
