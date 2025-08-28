1c158c8f70b21cb1074664579ad18b3774ef85fd
JancikK <yonaskollegaev@gmail.com>
2025-08-28 21:18:19 +0300

    deploy changed

--- terraform/main.tf
+++ terraform/main.tf
@@ -93,7 +93,7 @@ resource "aws_iam_role" "ssm_role" {
       Principal = {
         Federated = aws_iam_openid_connect_provider.github.arn
       }
-      Action = "sts:AssumeRole",
+      Action = "sts:AssumeRoleWithWebIdentity",
       Condition = {
         StringLike = { "token.actions.githubusercontent.com:sub" = "repo:tomas94tomas/Mock-Gatus:*"
         }
