57ce3b9a79fa858e6342d6943dadf1724d03508a
Marius Urbonas <marius.urbonas100@gmail.com>
2025-09-01 20:02:06 +0300

    try again

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "devops-project-terraform-state-2025"
-    key    = "terraform/terraform.tfstate"  # Fix "teraform" typo
+    key    = "terraform/terraform.tfstate" 
     region = "eu-central-1"
     encrypt = true
   }
