e538feb755f41030443d6837e75f07decd96be08
Marius Urbonas <marius.urbonas100@gmail.com>
2025-09-01 21:13:53 +0300

    git reset to previous state

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "devops-project-terraform-state-2025"
-    key          = "teraform/devops-project-terraform-state-2025/terraform.tfstate"
-    region       = "eu-central-1"
-    use_lockfile = true
-    encrypt      = true
+    bucket  = "devops-project-terraform-state-2025"
+    key     = "terraform/terraform.tfstate"
+    region  = "eu-central-1"
+    encrypt = true
   }
 }
